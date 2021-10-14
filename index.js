9ba62270aa07b6405d9fe2a5501cdbcf3ead8640
Ol_Kuts <kuts.ol.vas@gmail.com>
2021-10-14 09:29:51 +0300

    done: task-3

--- index.js
+++ index.js
@@ -12,47 +12,83 @@ const PORT = 4000;
 let maxId = getMaxId(todos);
 
 app.get('/notes/stats', (req, res) => {
-  const result = getStatistics(todos, categories);
-  res.status(200).json(result).end();
+  try {
+    const result = getStatistics(todos, categories);
+    res.status(200).json(result).end()
+  } catch (e) {
+    res.status(500).send('Server error').end();
+  }
 })
 
 app.get('/notes/:id', validation(['id']), (req, res) => {
-  const out = todos.filter(item => item.id === req.params.id)[0]
-  res.status(200).json(out).end();
+  try {
+    const out = todos.filter(item => item.id === req.params.id)[0]
+    out
+      ? res.status(200).json(out).end()
+      : res.status(404).send('Data not found').end();
+  } catch (e) {
+    res.status(500).send('Server error').end();
+  }
 })
 
 app.get('/notes', (req, res) => {
-  res.status(200).json(todos).end();
+  try {
+    todos.length
+      ? res.status(200).json(todos).end()
+      : res.status(200).json([]).end()
+  } catch (e) {
+    res.status(500).send('Server error').end();
+  }
 })
 
 app.post('/notes', validation(['body']), (req, res) => {
-  const newTodo = req.body;
-  maxId += 1;
-  newTodo.id = String(maxId);
-  newTodo.active = true;
-  newTodo.created = Date.now();
-  todos.push(newTodo);
-  res.status(200).json(todos).end();
+  try {
+    const newTodo = req.body;
+    maxId += 1;
+    newTodo.id = String(maxId);
+    newTodo.active = true;
+    newTodo.created = Date.now();
+    todos.push(newTodo);
+    res.status(200).json(todos).end();
+  } catch (e) {
+    res.status(500).send('Server error').end();
+  }
 })
 
 app.delete('/notes/:id', validation(['id']), (req, res) => {
-  for (let i = 0; i < todos.length; i++) {
-    if (todos[i].id === req.params.id) {
-      todos.splice(i, 1);
-      break;
+  try {
+    let isDelete = false;
+    for (let i = 0; i < todos.length; i++) {
+      if (todos[i].id === req.params.id) {
+        todos.splice(i, 1);
+        isDelete = true;
+        break;
+      }
     }
+    isDelete
+      ? res.status(200).json(todos).end()
+      : res.status(404).send('Data not found').end();
+  } catch (e) {
+    res.status(500).send('Server error').end();
   }
-  res.status(200).json(todos).end();
 })
 
 app.patch('/notes/:id', validation(['id', 'body']), (req, res) => {
-  for (let i = 0; i < todos.length; i++) {
-    if (todos[i].id === req.params.id) {
-      todos[i] = {...todos[i], ...req.body};
-      break;
+  try {
+    let isPatch = false;
+    for (let i = 0; i < todos.length; i++) {
+      if (todos[i].id === req.params.id) {
+        todos[i] = {...todos[i], ...req.body};
+        isPatch = true;
+        break;
+      }
     }
+    isPatch
+      ? res.status(200).json(todos).end()
+      : res.status(404).send('Data not found').end();
+  } catch (e) {
+    res.status(500).send('Server error').end();
   }
-  res.status(200).json(todos).end();
 })
 
 
